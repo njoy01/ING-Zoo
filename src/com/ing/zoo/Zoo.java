676311935ffa26d983620a04048cfe3b4b66c73b
N'Joy Schutte <njoy.schutte@hva.nl>
2025-12-15 02:51:45 +0100

    Refactored Zoo to support hello and hello <name> commands

--- src/com/ing/zoo/Zoo.java
+++ src/com/ing/zoo/Zoo.java
@@ -1,38 +1,44 @@
 package com.ing.zoo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Zoo {
-    public static void main(String[] args)
-    {
-        String[] commands = new String[4];
-        commands[0] = "hello";
-        commands[1] = "give leaves";
-        commands[2] = "give meat";
-        commands[3] = "perform trick";
+    public static void main(String[] args) {
+        List<Animal> animals = new ArrayList<>();
 
-        Lion henk = new Lion();
-        henk.name = "henk";
-        Hippo elsa = new Hippo();
-        elsa.name = "elsa";
-        Pig dora = new Pig();
-        dora.name = "dora";
-        Tiger wally = new Tiger();
-        wally.name = "wally";
-        Zebra marty = new Zebra();
-        marty.name = "marty";
+        animals.add(new Lion("Henk"));
+        animals.add(new Hippo("Hlsa"));
+        animals.add(new Pig("Dora"));
+        animals.add(new Tiger("Wally"));
+        animals.add(new Zebra("Marty"));
+        animals.add(new Bear("Winnie"));
+        animals.add(new Cow("Paula"));
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Voer uw command in: ");
+        String input = scanner.nextLine().trim().replaceAll("\\s+", " ");
 
-        String input = scanner.nextLine();
-        if(input.equals(commands[0] + " henk"))
-        {
-            henk.sayHello();
-        }
-        else
-        {
-            System.out.println("Unknown command: " + input);
+        if (input.equals("hello")) {
+            for (Animal animal : animals) {
+                animal.sayHello();
+            }
+        } else if (input.startsWith("hello ")) {
+            String name = input.substring(6);
+            boolean found = false;
+
+            for (Animal animal : animals) {
+                if (animal.getName().equalsIgnoreCase(name)) {
+                    animal.sayHello();
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                System.out.println("No animal with the name " + name + " found.");
+            }
         }
     }
 }
